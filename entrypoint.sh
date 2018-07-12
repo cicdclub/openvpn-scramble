89feb872d9c17b1671fc20a40452715de5101596
inetshell <inetshell@gmail.com>
2018-07-12 00:44:03 -0500

    Display OpenVPN log to stdout.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,16 @@ DHPARAM_FILE="/etc/openvpn/dhparam.pem"
 DHPARAM_SIZE="1024"
 OPENVPN_CONF="/etc/openvpn/server.conf"
 
+# Create symlink to stdout
+rm -rf /var/log/openvpn.log
+ln -s /dev/stdout /var/log/openvpn.log
+
 # Create DHPARAM file
 if [[ ! -f "${DHPARAM_FILE}" ]]; then
 	openssl dhparam -out "${DHPARAM_FILE}" "${DHPARAM_SIZE}"
 fi
 
+# Run server
 if [[ -f "${OPENVPN_CONF}" ]]; then
 	/usr/local/sbin/openvpn --config "${OPENVPN_CONF}"
 else
