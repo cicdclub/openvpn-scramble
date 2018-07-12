67be667a6845b03534b23881497d6e1e052715af
inetshell <inetshell@gmail.com>
2018-07-12 01:28:54 -0500

    Add iptables rules

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,16 @@ if [[ ! -f "${DHPARAM_FILE}" ]]; then
 	openssl dhparam -out "${DHPARAM_FILE}" "${DHPARAM_SIZE}"
 fi
 
+
+
+
 # Run server
 if [[ -f "${OPENVPN_CONF}" ]]; then
-	/usr/local/sbin/openvpn --config "${OPENVPN_CONF}"
+	/usr/local/sbin/openvpn --config "${OPENVPN_CONF}" &
+	# Add firewall rules
+	iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+	iptables -A FORWARD -i eth0 -o ovpns1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+	iptables -A FORWARD -i ovpns1 -o eth0 -j ACCEPT
 else
 	/bin/bash
 fi
